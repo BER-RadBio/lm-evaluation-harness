1ab060762c1bf3222fe4b8813583a1c5171cec7f
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2022-03-02 14:03:57 -0700

    add additional tasks, split up

--- lm_eval/tasks/radbio.py
+++ lm_eval/tasks/radbio.py
@@ -7,14 +7,11 @@ from pathlib import Path
 import pickle
 from lm_eval.metrics import mean, perplexity, matthews_corrcoef, f1_score
 
-class RadBio(Task):
-    # insert path name here
-    # DATASET_PATH = Path("/homes/mzvyagin/isInSystemQA.obj")
-    VERSION=1.0
 
-    def download(self):
-        # some kind of unpickling call here? I don't think we can download from the internet as it's not public
-        pass
+class RadBio(Task):
+    """Base Class for yes/no questions"""
+    DATASET_PATH = Path("./radbio_data")
+    VERSION = 1.0
 
     def has_training_docs(self):
         return False
@@ -24,23 +21,6 @@ class RadBio(Task):
 
     def has_test_docs(self):
         return False
-
-    def training_docs(self):
-    #     # These should return a Python iterable (list or generator) of dicts that can be queried for individual doc
-    #     # examples. NOTE: If your task doesn't have a train/validation/test set, remember to raise a NotImplementedError
-    #     # for that specific split.
-    #     # load in the training data from the pickle and return as iterable
-        with open("/homes/mzvyagin/radbio/data/isInSystemQAlarge/train.pkl", "rb") as f:
-            data = pickle.load(f)
-        return data
-
-    def validation_docs(self):
-        with open("/homes/mzvyagin/radbio/data/isInSystemQAlarge/test.pkl", "rb") as f:
-            data = pickle.load(f)
-        return data
-
-    def test_docs(self):
-        return NotImplementedError
 
     def doc_to_text(self, doc):
         return doc["question"]
@@ -76,3 +56,50 @@ class RadBio(Task):
             "acc": mean,
             "f1": f1_score
         }
+
+
+class isInSystemQA(RadBio):
+
+    def download(self):
+        pass
+
+    def training_docs(self):
+        with open("/homes/mzvyagin/radbio/data/isInSystemQAlarge/train.pkl", "rb") as f:
+            data = pickle.load(f)
+        return data
+
+    def validation_docs(self):
+        with open("/homes/mzvyagin/radbio/data/isInSystemQAlarge/test.pkl", "rb") as f:
+            data = pickle.load(f)
+        return data
+
+    def test_docs(self):
+        return NotImplementedError
+
+
+class goAHumanQA(RadBio):
+    def download(self):
+        pass
+
+    def training_docs(self):
+        return NotImplementedError
+
+    def validation_docs(self):
+        return NotImplementedError
+
+    def test_docs(self):
+        return NotImplementedError
+
+
+class goARadiationResponseQA(RadBio):
+    def download(self):
+        pass
+
+    def training_docs(self):
+        return NotImplementedError
+
+    def validation_docs(self):
+        return NotImplementedError
+
+    def test_docs(self):
+        return NotImplementedError
