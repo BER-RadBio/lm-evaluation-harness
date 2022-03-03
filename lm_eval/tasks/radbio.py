ed1361fcd6ff3dbfd4eb21196ddc90b373608abd
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2022-03-03 14:21:57 -0700

    update to download dataet

--- lm_eval/tasks/radbio.py
+++ lm_eval/tasks/radbio.py
@@ -6,6 +6,8 @@ from lm_eval.base import Task, rf
 from pathlib import Path
 import pickle
 from lm_eval.metrics import mean, perplexity, matthews_corrcoef, f1_score
+from best_download import download_file
+from zipfile import ZipFile
 
 
 class RadBio(Task):
@@ -13,11 +15,24 @@ class RadBio(Task):
     DATASET_PATH = Path("./radbio_data")
     VERSION = 1.0
 
+    def download(self):
+        if self.DATASET_PATH.exists():
+            # don't re-download the dataset
+            return
+        Path.mkdir(self.DATASET_PATH, parents=True)
+        url = "https://docs.google.com/uc?export=download&id=1o2LMR5xdNlTcj2qpSlWEwLPseJxm4AXU"
+        checksum = "c78104ee5aaff4339ed9bd30526a01063eccb89765c9842c53de8f10b8accb32"
+        zip_path = self.DATASET_PATH / "radbio_question_sets.zip"
+        download_file(url, local_file=str(zip_path), expected_checksum=checksum)
+        with ZipFile(zip_path, "r") as zip:
+            zip.extractall(self.DATASET_PATH)
+        os.remove(zip_path)
+
     def has_training_docs(self):
         return False
 
     def has_validation_docs(self):
-        return True
+        return False
 
     def has_test_docs(self):
         return False
@@ -60,16 +75,13 @@ class RadBio(Task):
 
 class isInSystemQA(RadBio):
 
-    def download(self):
-        pass
-
     def training_docs(self):
-        with open("/homes/mzvyagin/radbio/data/isInSystemQAlarge/train.pkl", "rb") as f:
+        with open(self.DATASET_PATH / "isInSystemQA/train.pkl", "rb") as f:
             data = pickle.load(f)
         return data
 
     def validation_docs(self):
-        with open("/homes/mzvyagin/radbio/data/isInSystemQAlarge/test.pkl", "rb") as f:
+        with open(self.DATASET_PATH / "isInSystemQA/test.pkl", "rb") as f:
             data = pickle.load(f)
         return data
 
@@ -78,28 +90,65 @@ class isInSystemQA(RadBio):
 
 
 class goAHumanQA(RadBio):
-    def download(self):
-        pass
 
     def training_docs(self):
-        return NotImplementedError
+        with open(self.DATASET_PATH / "goAHumanQA/train.pkl", "rb") as f:
+            data = pickle.load(f)
+        return data
 
     def validation_docs(self):
-        return NotImplementedError
+        with open(self.DATASET_PATH / "goAHumanQA/test.pkl", "rb") as f:
+            data = pickle.load(f)
+        return data
 
     def test_docs(self):
         return NotImplementedError
 
 
 class goARadiationResponseQA(RadBio):
-    def download(self):
-        pass
 
     def training_docs(self):
-        return NotImplementedError
+        with open(self.DATASET_PATH / "goARadiationResponseQA/train.pkl", "rb") as f:
+            data = pickle.load(f)
+        return data
 
     def validation_docs(self):
+        with open(self.DATASET_PATH / "goARadiationResponseQA/test.pkl", "rb") as f:
+            data = pickle.load(f)
+        return data
+
+    def test_docs(self):
         return NotImplementedError
+
+
+class ppiHumanQA(RadBio):
+    """Protein protein interaction dataset"""
+
+    def training_docs(self):
+        with open(self.DATASET_PATH / "ppiHumanQA/train.pkl", "rb") as f:
+            data = pickle.load(f)
+        return data
+
+    def validation_docs(self):
+        with open(self.DATASET_PATH / "ppiHumanQA/test.pkl", "rb") as f:
+            data = pickle.load(f)
+        return data
+
+    def test_docs(self):
+        return NotImplementedError
+
+
+class humanPathwaysQA(RadBio):
+
+    def training_docs(self):
+        with open(self.DATASET_PATH / "humanPathwaysQA/train.pkl", "rb") as f:
+            data = pickle.load(f)
+        return data
+
+    def validation_docs(self):
+        with open(self.DATASET_PATH / "humanPathwaysQA/test.pkl", "rb") as f:
+            data = pickle.load(f)
+        return data
 
     def test_docs(self):
         return NotImplementedError
